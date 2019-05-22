ee58b0b749d8b07df40850d24c38e4fdf1fca9bf
Helberth Lucas <techelberthlucas@gmail.com>
2019-05-21 21:52:12 -0300

    Exercicio 02

---
+++ exercicio03/js/script.js
@@ -0,0 +1,22 @@
+
+
+
+var n1 = prompt("Informe a 1 nota do aluno");
+var n2 = prompt("Informe a 2 nota do aluno");
+var n3 = prompt("Informe a 3 nota do aluno");
+var n4 = prompt("Informe a 4 nota do aluno");
+
+var somatorio = parseInt(n1)+parseInt(n2)+parseInt(n3)+parseInt(n4);
+var media = somatorio/4;
+
+if(somatorio >= 65)
+    {
+        alert("Aluno Aprovado ");        
+    }
+    else{
+         alert("Aluno Reprovado  ");
+        }   
+
+
+
+
