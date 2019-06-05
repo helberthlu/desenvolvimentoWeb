e48db9f5d24002ae3e79d6c8b0433090ec8f1a33
Helberth Lucas <techelberthlucas@gmail.com>
2019-06-04 21:30:54 -0300

    Completando pratica6

--- pratica06/js/script.js
+++ pratica06/js/script.js
@@ -14,33 +14,32 @@ function imc(_altura , _peso)
     var m = _peso / (_altura * _altura);
     return m.toFixed(2);
 }
-input.value = nome +" "+ imc(altura,peso)+" " ;
 
 if(imc(altura,peso) < 16)
 {
     resultado = "BAIXO PESO MUITO GRAVE";
 }
-else if(imc(altura,peso) >= 16 && imc(altura,peso) < 16,99 )
+else if(imc(altura,peso) >= 16 && imc(altura,peso) <= 16.99 )
 {
     resultado = "BAIXO PESO GRAVE";
 }
-else if(imc(altura,peso) >= 17 && imc(altura,peso) < 18,49 )
+else if(imc(altura,peso) >= 17 && imc(altura,peso) <= 18.49 )
 {
     resultado = "BAIXO PESO";
 }
-else if(imc(altura,peso) >= 18,50 && imc(altura,peso) <= 24,99 )
+else if(imc(altura,peso) >= 18,50 && imc(altura,peso) <= 24.99 )
 {
     resultado = "PESO NORMAL";
 }
-else if(imc(altura,peso) >= 25 && imc(altura,peso) <= 29,99 )
+else if(imc(altura,peso) >= 25 && imc(altura,peso) <= 29.99 )
 {
     resultado = "SOBRE PESO";
 }
-else if(imc(altura,peso) >= 30 && imc(altura,peso) <= 34,99 )
+else if(imc(altura,peso) >= 30 && imc(altura,peso) <= 34.99 )
 {
     resultado = "OBSIDADE GRAU I";
 }
-else if(imc(altura,peso) >= 35 && imc(altura,peso) <= 39,99 )
+else if(imc(altura,peso) >= 35 && imc(altura,peso) <= 39.99 )
 {
     resultado = "OBSIDADE GRAU II";
 }
